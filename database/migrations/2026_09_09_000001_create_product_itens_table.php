824e25006ddea83a0a7116535cf2dc9d4f097feb
PEN-g-UIM <stop0854@gmail.com>
2026-09-09 20:00:21 -0300

    Criando migration de itens dos produtos

---
+++ database/migrations/2026_09_09_000001_create_product_itens_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('product_itens', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+            $table->decimal('quantidade', 10, 2);
+            $table->string('cor');
+            $table->decimal('valor', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('product_itens');
+    }
+};
